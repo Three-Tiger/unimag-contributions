10462241a4915f1483539a8672c873d392a14e3a
DuongntGCC200026 <duongntgcc200026@fpt.edu.vn>
2024-03-12 23:31:08 +0700

    Function for Statictis

---
+++ UniMagContributions/Repositories/Interface/IStatisticRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace UniMagContributions.Repositories.Interface
+{
+	public interface IStatisticRepository
+	{
+		Dictionary<string, Dictionary<string, int>> GetContributionsByFacultyAndAcademicYear();
+		Dictionary<string, Dictionary<string, double>> GetPercentageContributionsByFacultyAndAcademicYear();
+		public Dictionary<string, Dictionary<string, int>> GetNumberOfContributorsByFacultyAndAcademicYear();
+	}
+}
